b25bf601bf433137b296d1b2791fd934a2a04274
Maria Genis <magegr@alumnatflorida.es>
2024-11-05 16:22:46 +0100

    Ejercicio 2 terminado

--- ej_2.php
+++ ej_2.php
@@ -7,6 +7,21 @@
 <body>
     <?php
     /*Escribe un script para encontrar los duplicados sin usar funciones de array. Coloca estos valores previamente en un array.*/
+    $duplicados=[1,2,3,3,4,5,5,6];
+    $numeros=[];
+    for($i=0;$i<count($duplicados);$i++){
+        for($j=0;$j<count($duplicados);$j++){
+            if ($j!==$i && ($duplicados[$i]==$duplicados[$j])){
+                $numeros[]=$duplicados[$i];
+                break;
+            }
+    }
+}
+
+var_dump($numeros);
+    
+    
+    
     ?>
 </body>
 </html>
