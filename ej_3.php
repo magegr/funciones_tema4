597340d935b9e99bc6d1d93ac6daa1c62098b13e
Maria Genis <magegr@alumnatflorida.es>
2024-11-05 16:40:34 +0100

    terminado ejercicio 3

--- ej_3.php
+++ ej_3.php
@@ -6,7 +6,22 @@
 </head>
 <body>
     <?php
-    /*Escribe un script separar los valores de un array en dos arrays: los pares en uno y en el otro los impares.*/
-    ?>
+    /*Escribe un script separar los valores de un array en dos arrays: 
+    los pares en uno y en el otro los impares.*/
+    $numeros=[1,2,3,4,5,6,7,8,9];
+    $pares=[];
+    $impares=[];
+    
+    for($i=0;$i<count($numeros);$i++){
+        $resto=$numeros[$i]%2;
+        if($resto == 0){
+            $pares[]=$numeros[$i];
+        }else{
+            $impares[]=$numeros[$i];
+        }
+    }
+    var_dump($pares);
+    var_dump($impares);
+   ?>
 </body>
 </html>
