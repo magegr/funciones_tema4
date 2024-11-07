043769c20c024610784737ac1150831c13ce7f1c
Maria Genis <magegr@alumnatflorida.es>
2024-11-07 10:45:49 +0100

    terminado ejercicio 6

--- ej_5.php
+++ ej_5.php
@@ -9,6 +9,23 @@
     /*Crea un script para contar cuantas repeticiones de cada 
     valor aparecen en un array. Puedes usar la función in_array 
     y array_push si las consideras aplicable..*/
+    $numeros=[1,6,3,4,5,6,6,8,9];
+    $numeros_repetidos=[];
+  
+    for($i=0;$i<count($numeros);$i++){
+        $contador=0;
+       if(in_array($numeros[$i], $numeros_repetidos)){
+           continue;
+       }
+        for($j=0;$j<count($numeros);$j++){
+            if($numeros[$i] == $numeros[$j] && ($i!== $j)){
+                $numeros_repetidos[] = $numeros[$i];
+                $contador++;
+            }
+        }
+        echo "el numero ".$numeros[$i] ." se repite <br>"; 
+        var_dump($numeros_repetidos);
+    }
     ?>
 </body>
 </html>
--- ej_6.php
+++ ej_6.php
@@ -6,8 +6,32 @@
 </head>
 <body>
     <?php
-    /*Haz un array con 10 notas. A continuación, elimina la nota mas baja y la más alta (solo una, en caso de estar repetida). Haz la media de las que quedan. Puedes comprobar si los datos són numéricos con la función is_numeric.
-     Además puedes usar también array_splice y implode si te son útiles.*/
+    /*Haz un array con 10 notas. A continuación, elimina la nota mas baja y la más alta (solo una, en caso de estar repetida). 
+    Haz la media de las que quedan. Puedes comprobar si los datos són numéricos con la función is_numeric.
+    Además puedes usar también array_splice y implode si te son útiles.*/
+    $notas=["maria"=> 5,"pedro"=> 8,"andrea"=> 5,"jose"=> 9,"sara"=> 8,"pepe"=> 7,"anastasia"=> 9];
+    $minimo= min($notas);
+    $maximo=max($notas);
+    $eliminados=false;
+    $eliminados2=false;
+    $media=0;
+    foreach($notas as $nombre=>$nota){
+        $posicion1=array_search($minimo,array_values($notas));
+        $posicion2=array_search($maximo,array_values($notas));
+        if($eliminados !== true && $nota==$minimo){
+                array_splice($notas,$posicion1,1);
+                $eliminados=true;
+        }elseif($eliminados2 !== true && $nota==$maximo){
+                array_splice($notas,$posicion2,1);
+                $eliminados2=true;
+            }
+        }
+       $media= array_sum($notas)/count($notas); 
+      echo "la media de las notas es $media";
+      var_dump($notas);
+
+
+
     ?>
 </body>
 </html>
